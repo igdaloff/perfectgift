da5a556b58192cb8aa643a2d1efd6e9a32606029
igdaloff <louis.nathan@gmail.com>
2015-06-19 23:34:04 -0400

    setting up twilio test

--- sms-test.php
+++ sms-test.php
@@ -3,5 +3,5 @@
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>
 <Response>
-    <Message>Hello, Mobile Monkey</Message>
+    <Message>Welcome! I'm Nathan, here to help you find that perfect gift. Please describe your relationship with the gift recipient.</Message>
 </Response>
